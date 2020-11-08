322b7dc258a812059400dca73a2db19a3e0dbf68
cane21 <cane21@kookmin.ac.kr>
2020-11-08 22:33:41 +0900

    updeta GameScene

---
+++ Assets/Resources/Scripts/ComponentMng.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentMng : MonoBehaviour
+{
+    public GameObject[] temp;
+    public Transform[] ComponentRoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (temp[i] != null)
+                Instantiate(temp[i], ComponentRoot[i].localPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
