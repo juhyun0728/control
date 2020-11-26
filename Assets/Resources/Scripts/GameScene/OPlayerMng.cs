d4fb2491ba01dab4068a85204ba43604d45de89e
cane21 <cane21@kookmin.ac.kr>
2020-11-26 21:23:02 +0900

    update change

---
+++ Assets/Resources/Scripts/GameScene/OPlayerMng.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OPlayerMng : MonoBehaviour, IDropHandler
+{
+    float fSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(true)
+        {
+            moveObject();
+        }
+
+        if(true)
+        {
+
+        }
+    }
+
+    void moveObject()
+    {
+        float keyHorizontal = Input.GetAxis("Horizontal");
+
+        //float keyVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * fSpeed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+
+        //transform.Translate(Vector3.up * fSpeed * Time.smoothDeltaTime * keyVertical, Space.World);
+    }
+
+    void jumpObject()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("FF");
+        }
+        Debug.Log("FASDASD");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("FF");
+    }
+    
+}
