90626f1f2cbc97c74f61fd4a4b93c99bea7c854f
juhyun0728 <juhyun0728@naver.com>
2020-11-26 11:45:40 +0900

    맵 대충 그려보기

---
+++ Assets/Resources/Scripts/GameScene/Switch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject door;
+    void OnTriggerEnter2D (Collider2D other) {
+    	if (other.gameObject.tag == "Object") {
+    		door.SetActive(false);
+    		this.transform.localScale = new Vector3(1,3,3);
+    	}
+    }
+
+    void OnTriggerExit2D (Collider2D other) {
+    	door.SetActive(true);
+    	this.transform.localScale = new Vector3(3,3,3);
+    }
+}
